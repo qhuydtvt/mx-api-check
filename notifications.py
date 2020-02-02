95f7d8f65e45a054eab48ca7f7f8840f1cd01d95
huynq.work <huynq.work@gmail.com>
2020-02-03 02:09:18 +0700

    Fix bug not sending notification

--- notifications.py
+++ notifications.py
@@ -2,10 +2,10 @@ import requests
 
 def create_notification(notification_type, notification_config):
   if notification_type == 'httpEndpoint':
-    url = notification_config.url
+    noti_url = notification_config.url
     def end_point_notify(json):
-      nonlocal url
-      response = requests.post(url, json=json)
+      nonlocal noti_url
+      response = requests.post(noti_url, json=json)
       if response.status_code >= 400:
         print(response)
         raise Exception('Notication failed')
@@ -14,16 +14,19 @@ def create_notification(notification_type, notification_config):
     chat_id = notification_config.chatId
     bot_id = notification_config.botId
     bot_token = notification_config.botToken
-    url = f'https://api.telegram.org/bot{bot_id}:{bot_token}/sendMessage'
+    noti_url = f'https://api.telegram.org/bot{bot_id}:{bot_token}/sendMessage'
     def telegram_notify(input_json):
-      nonlocal url
+      nonlocal noti_url
       nonlocal chat_id
+      api_url = input_json['url']
+      error = input_json['error']
+      message = f'{api_url} failed.\nDetails: {error}'
       json = {
         'chat_id': chat_id,
-        'text': input_json['error'],
+        'text': message,
         'parse_mode': 'Markdown'
       }
-      response = requests.post(url, json=json)
+      response = requests.post(noti_url, json=json)
       if response.status_code >= 400:
         print(response)
         raise Exception('Notication failed')
